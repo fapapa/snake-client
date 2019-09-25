d791e6ee97d805c8a9be5214acec7f3844e4a432
Fabio Papa <fabtheman@gmail.com>
2019-09-25 14:12:11 -0700

    Add ability to handle user input
    So far just handling `ctrl+c` to exit

--- client.js
+++ client.js
@@ -23,4 +23,22 @@ const connect = () => {
   return conn;
 };
 
+const handleUserInput = (input) => {
+  if (input === "\u0003") {
+    console.log("Exiting...");
+    process.exit();
+  }
+};
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+setupInput();
+
+
 module.exports = { connect };
