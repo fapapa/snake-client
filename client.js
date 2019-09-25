b0d854b316f6b774e9f5b1184bd8068dd7e979a8
Fabio Papa <fabtheman@gmail.com>
2019-09-25 15:03:44 -0700

    Refactor: Move constants into their own module

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 const net = require('net');
+const { IP, PORT } = require('./constants.js');
 
 const connect = () => {
   const conn = net.createConnection({
-    host: '192.168.88.254',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.setEncoding('utf8');
 
   conn.on('connect', () => {
-    console.log("Successfully connected to the game serevr");
+    console.log("Successfully connected to the game server");
     conn.write("Name: FAB");
     setInterval(() => {
       // conn.write("Move: up");
