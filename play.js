d4b6c6c518be05cb4d1e50aed75f4dca05f264fb
Fabio Papa <fabtheman@gmail.com>
2019-09-25 13:11:03 -0700

    Added client code to connect to snek server, log messages received

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '192.168.88.254',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting...');
+connect();
