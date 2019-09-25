2aab78140c86b2b8c3dcc63ba02b376695271171
Fabio Papa <fabtheman@gmail.com>
2019-09-25 13:21:18 -0700

    Refactor the connection code into its own module

--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: '192.168.88.254',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
+const { connect } = require('./client.js');
 console.log('Connecting...');
 connect();
