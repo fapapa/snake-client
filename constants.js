7356c3a475f33546711209063c9a6cb97f77a13a
Fabio Papa <fabtheman@gmail.com>
2019-09-25 15:17:56 -0700

    Refactor: More constants!

--- constants.js
+++ constants.js
@@ -1,7 +1,20 @@
 const IP = '192.168.88.254';
 const PORT = 50541;
 
+const MOVE_UP_KEY = "c";
+const MOVE_LEFT_KEY = "h";
+const MOVE_DOWN_KEY = "t";
+const MOVE_RIGHT_KEY = "n";
+const SAY_NAHA_KEY = "m";
+const SAY_VROOOM_KEY = "v";
+
 module.exports = {
   IP,
-  PORT
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_DOWN_KEY,
+  SAY_NAHA_KEY,
+  SAY_VROOOM_KEY
 };
--- input.js
+++ input.js
@@ -1,20 +1,31 @@
+const {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_DOWN_KEY,
+  SAY_NAHA_KEY,
+  SAY_VROOOM_KEY
+} = require('./constants.js');
+
 let connection;
 
 const handleUserInput = (input) => {
   if (input === "\u0003") {
     console.log("Exiting...");
     process.exit();
-  } else if (input === "c") {
+  } else if (input === MOVE_UP_KEY) {
     connection.write("Move: up");
-  } else if (input === "h") {
+  } else if (input === MOVE_LEFT_KEY) {
     connection.write("Move: left");
-  } else if (input === "t") {
+  } else if (input === MOVE_DOWN_KEY) {
     connection.write("Move: down");
-  } else if (input === "n") {
+  } else if (input === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
-  } else if (input === "m") {
+  } else if (input === SAY_NAHA_KEY) {
     connection.write("Say: Nahaa!");
-  } else if (input === "v") {
+  } else if (input === SAY_VROOOM_KEY) {
     connection.write("Say: VROOOOM!");
   }
 };
